25377f8b7a53ca0b169e0502e3ec308c0f46dcc1
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-11-01 01:06:26 +0900

    Add JavaScript functions

--- firstapp/src/editor.js
+++ firstapp/src/editor.js
@@ -33,3 +33,90 @@ function onLoad() {
     return false;
   }
 };
+
+function openLoadFile() {
+  var win = browserWindow.getFocusedWindow();
+
+  dialog.showOpenDialog(
+      win,
+      {
+        properties: ["openFile"],
+        filters: [
+          {
+            name: "Documents",
+            extensions: ["txt", "text", "html", "js"]
+          }
+        ]
+      },
+      function(filenames) {
+        if (filenames) {
+          readFile(filenames[0]);
+        }
+      });
+}
+
+function readFile(path) {
+  currentPath = path;
+  fs.readFile(path, function(error, text) {
+    if (error != null) {
+      alert("error: " + error);
+      return;
+    }
+
+    footerArea.innerHTML = path;
+    editor.setValue(text.toString());
+  });
+}
+
+function saveFile() {
+  if (currentPath == "") {
+    saveNewFile();
+    return;
+  }
+
+  var win = browserWindow.getFocusedWindow();
+  dialog.showMessageBox(win, {
+    title: "ファイルの上書き保存を行います。",
+    type: "info",
+    buttons: ["OK", "Cancel"],
+    detail: "本当に保存しますか？"
+  },
+  function(response) {
+    if (response == 0) {
+      var data = editor.getValue();
+      writeFile(currentPath, data);
+    }
+  });
+}
+
+function writeFile() {
+  fs.writeFile(path, dta, function(error) {
+    if (error != null) {
+      alert("error: " + error);
+      return;
+    }
+  });
+}
+
+function saveNewFile() {
+  var win = browserWindow.getFocuseWindow();
+  dialog.showSaveDialog(
+      win,
+      {
+        properties: ["openFile"],
+        filters: [
+          {
+            name: "Documents",
+            extensions: ["txt", "text", "html", "js"]
+          }
+        ]
+      },
+      function(fileName) {
+        if (fileName) {
+          var data = editor.getValue();
+          currentPath = fileName;
+          writeFile(currentPath, data);
+        }
+      }
+  );
+}
