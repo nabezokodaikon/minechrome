995139887fe2b2338a6289cbd1b98f5b9d051647
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-11-10 02:44:16 +0900

    Add webview tag

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -5,13 +5,18 @@ const webViewCtl = require("./webview-controller.js");
 
 let webView = null;
 
-window.addEventListener("load", (e) => {
-  console.log("window load");
-}, false);
-
 ipcRenderer.on(webViewCtl.getChannel(), (e, message) => {
   console.log("Received: " + message);
 });
+
+window.addEventListener("load", (e) => {
+  console.log("window load");
+
+  const contents = document.getElementById("contents");
+  webView = document.createElement("webview");
+  webView.src= "https://www.github.com/";
+  contents.appendChild(webView);
+}, false);
 
 document.addEventListener("keydown", (e) => {
   console.log(e);
