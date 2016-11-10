ba99da6998f00e60b3a0aa7276eda3ed6fc1cd4f
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-11-11 06:51:49 +0900

    Add context menu

--- firstapp/src/index.js
+++ firstapp/src/index.js
@@ -1,7 +1,32 @@
 "use strict";
 
-const {ipcRenderer} = require("electron");
+const {remote, ipcRenderer} = require("electron");
+const {Menu, MenuItem} = remote;
 const webViewCtl = require("./webview-controller.js");
+
+const contextMenuTemplate = [
+  {
+    role: "undo"
+  },
+  {
+    role: "separator"
+  },
+  {
+    role: "copy"
+  },
+  {
+    role: "paste"
+  },
+  {
+    role: "separator"
+  },
+  {
+    label: "MenuItem1", click() {
+      console.log("item 1 clicked");
+    },
+  }
+];
+const contextMenu = Menu.buildFromTemplate(contextMenuTemplate);
 
 let webView = null;
 
@@ -14,8 +39,13 @@ window.addEventListener("load", (e) => {
 
   const contents = document.getElementById("contents");
   webView = document.createElement("webview");
-  webView.src= "https://www.github.com/";
+  webView.src= "https://www.google.co.jp/";
   contents.appendChild(webView);
+}, false);
+
+window.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+  contextMenu.popup(remote.getCurrentWindow());
 }, false);
 
 document.addEventListener("keydown", (e) => {
