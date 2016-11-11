01162511070a8038a5376ebed7f9970b510c5773
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-11-12 08:43:19 +0900

    Add URL string test function

--- src/string-util.js
+++ src/string-util.js
@@ -1,9 +1,14 @@
 "use strict"
 
+const urlRegExp = new RegExp(/^(https?|ftp)(:\/\/[-_.!~*\'()a-zA-Z0-9;\/?:¥@&=+\$,%#]+)$/);
+
 module.exports = {
   fixedEncodeURIComponent: function(str) {
     return encodeURIComponent(str).replace(/[!'()*]/g, function(c) {
       return '%' + c.charCodeAt(0).toString(16);
     });
+  },
+  isURL: function(str) {
+     return urlRegExp.test(str);
   }
 }
