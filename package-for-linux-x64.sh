ccde9926bae61d2584b302db4bfd2b63664386e6
nabezokodaikon <nabezoko.daikon@gmail.com>
2016-11-17 04:53:31 +0900

    Add packager overwrite option

--- package-for-linux-x64.sh
+++ package-for-linux-x64.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-electron-packager src minechrome --platform=linux --arch=x64 --version=1.4.6
+electron-packager src minechrome --platform=linux --arch=x64 --version=1.4.6 --overwrite=true
